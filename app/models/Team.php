d2607575681842d9a0c478f4060b4a94aac9deb9
hitandyrun <hitandyrun@users.noreply.github.com>
2014-10-13 13:11:49 +0300

    Update Team.php
    Added some relationships to Team model

--- app/models/Team.php
+++ app/models/Team.php
@@ -3,7 +3,15 @@
 class Team extends \Eloquent {
 	protected $table="teams";
 
-	protected $primaryKey = 'id';
+	protected $primaryKey = 'matchId';
 
-	
+	public function match()
+	{
+		return $this->belongsTo('Match','matchId','matchId');
+	}
+
+	public function participants() 
+	{
+		return $this->hasManyThrough('Participant','Match','matchId','matchId')->where('teamId','=',$this->teamId);
+	}
 }
